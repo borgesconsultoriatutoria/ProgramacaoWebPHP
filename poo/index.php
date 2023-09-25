913db1ad9be80f2ac771bf3835fb8b53daa501b8
borgesconsultoriatutoria <borgesconsultoriatutoria@gmail.com>
2023-09-25 19:37:48 -0300

    Update index.php

--- poo/index.php
+++ poo/index.php
@@ -7,7 +7,33 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
   </head>
   <body class="container">
-    <form
+    <form action="resposta.php" method="POST">
+        <div class="row">
+            <div class="col">
+                <input type="text" placeholder="Informe o nome"
+                    class="form-control" name="nome"/>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <input type="text" placeholder="Informe o endereço"
+                    class="form-control" name="endereco"/>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <input type="number" placeholder="Informe a idade"
+                    class="form-control" name="idade"/>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <button type="submit" class="btn btn-secondary">
+                    Enviar
+                </button>
+            </div>
+        </div>
+    </form>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
   </body>
 </html>
